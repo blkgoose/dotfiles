f18340bac2cc7495f9bfa7ead716c4b9dd2ebc3b
blkgoose <alessio.biancone@gmail.com>
2023-07-14 23:14:51 +0200

    feat(xmonad): add xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,19 @@
 import XMonad
 
-main = xmonad def
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Ungrab
+import XMonad.Layout.ThreeColumns
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+main = xmonad $ ewmhFullscreen . ewmh . xmobarProp $ myConfig
+
+myConfig = def
     { terminal = "alacritty"
+    , layoutHook = myLayout
     , borderWidth = 3
     }
+
+myLayout = ThreeColMid 1 (3/100) (1/2)
