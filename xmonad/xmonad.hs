f079b3097f59982ef4fdf4e160441fb74d5d7f5f
blkgoose <alessio.biancone@gmail.com>
2023-07-16 17:21:47 +0200

    feat(xmonad): add no border rule

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Ungrab
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.NoBorders
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog (xmobar)
 import XMonad.Hooks.StatusBar
@@ -12,7 +13,7 @@ import XMonad.Util.Run
 
 myConf = def {
             terminal = "alacritty"
-            , layoutHook = ThreeColMid 1 (3/100) (1/2)
+            , layoutHook = smartBorders $ ThreeColMid 1 (3/100) (1/2)
             , borderWidth = 2
             , modMask = mod4Mask
             }
