e54c60e6e0f38e244846455dfe5fea134df6278c
blkgoose <alessio.biancone@gmail.com>
2023-07-29 13:23:41 +0200

    style: format

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,6 @@
 import XMonad
 
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Ungrab
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.NoBorders
@@ -11,13 +11,15 @@ import XMonad.Hooks.StatusBar.PP
 import XMonad.Actions.UpdatePointer (updatePointer)
 import XMonad.Util.Run
 
-myConf = def {
-            terminal = "alacritty"
-            , layoutHook = smartBorders $ ThreeColMid 1 (3/100) (1/2)
-            , borderWidth = 2
-            , modMask = mod4Mask
-            }
-
+myConf = def { terminal = "alacritty"
+             , layoutHook = smartBorders $ ThreeColMid 1 (3/100) (1/2)
+             , borderWidth = 2
+             , modMask = mod4Mask
+             }
+            `additionalKeys`
+            [
+                ((mod4Mask, xK_o), spawn "echo $PATH > /home/alessio/path_out")
+            ]
 
 main = do
     xmonad =<< xmobar (ewmh . ewmhFullscreen $ myConf)
